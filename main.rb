00713b3a6abed709353079d1e27d0cabe0e646ba
Carly Ho <me@carlymho.com>
2013-04-24 13:45:01 -0500

    Add some views and stuff

--- main.rb
+++ main.rb
@@ -3,6 +3,19 @@ require 'sinatra'
 
 set :sessions, true
 
+get '/' do
+	erb :home
+end
 
+get '/new_player' do
+	erb :new_player
+end
 
+post '/new_player' do
+	if params[:name].nil? or params[:name].empty?
+		@error = "You must enter your name."
+		erb :new_player
+	else
 
+	end
+end
