c77b82505aba9f3ffa8e15c7d8265975ae94a212
Carly Ho <me@carlymho.com>
2013-04-26 15:56:45 -0500

    Add game logic, images, etc.

--- main.rb
+++ main.rb
@@ -3,10 +3,17 @@ require 'sinatra'
 
 set :sessions, true
 
+# STATUSES, FOR MY REFERENCE:
+# 0 = Nothing interesting has happened yet
+# 1 = Blackjack!
+# 2 = Total score exactly equal to 21
+# 3 = Staying
+# 4 = Busted
+
 helpers do
 	def setup_game
-		suits = ["H", "D", "S", "C"]
-		values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+		suits = ["hearts", "diamonds", "spades", "clubs"]
+		values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
 		deck = []
 		suits.each do |suit|
 			values.each do |value|
@@ -18,15 +25,77 @@ helpers do
 		session[:dealer_hand] = session[:deck].pop(2)
 	end
 
+	def get_card_image(card)
+		return card[0]+"_"+card[1].to_s+".jpg"
+	end
+
+	def print_hand(hand)
+		images = ""
+		hand.each do |card|
+			images += "<img src='/images/cards/#{get_card_image(card)}' class='img-rounded' />"
+		end
+		return images
+	end
+
+	def print_dealer_hand
+		hand = session[:dealer_hand]
+		images = ""
+		cards_in_hand = hand.length
+		hand.each_with_index do |card, i|
+			if i != cards_in_hand-1
+				images += "<img src='/images/cards/cover.jpg' class='img-rounded' />"
+			else
+				images += "<img src='/images/cards/#{get_card_image(card)}' class='img-rounded' />"
+			end
+		end
+		return images
+	end
+
+	def determine_winner
+		if session[:player_status] == 1
+			if session[:dealer_status] == 1
+				session[:current_winner] = "tie"
+			else
+				session[:current_winner] = "player"
+			end
+		elsif session[:dealer_status] == 1
+			session[:current_winner] = "dealer"
+		elsif session[:player_status] == 2
+			if session[:dealer_status] == 2
+				if session[:player_hand].length < session[:dealer_hand].length
+					session[:current_winner] = "player"
+				elsif session[:playerhand.length] > session[:dealer_hand].length
+					session[:current_winner] = "dealer"
+				else
+					session[:current_winner] = "tie"
+				end
+			else
+				session[:current_winner] = "player"
+			end
+		elsif session[:player_status] == 3 and session[:dealer_status] == 3
+			if session[:player_score] > session[:dealer_score]
+				session[:current_winner] = "player"
+			elsif session[:dealer_score] > session[:player_score]
+				session[:current_winner] = "dealer"
+			else
+				session[:current_winner] = "tie"
+			end
+		elsif session[:player_status] == 4 and session[:dealer_status] != 4
+			session[:current_winner] = "dealer"
+		elsif session[:player_status] != 4 and session[:dealer_status] == 4
+			session[:current_winner] = "player"
+		end
+	end
+
 	def calculate_total(hand)
 		total = 0
 		ace_count = 0
 		hand.each do |card|
 			value = card[1]
-			if value == "A"
+			if value == "ace"
 				total += 11
 				ace_count += 1
-			elsif value == ("J" or "Q" or "K")
+			elsif value == ("jack" or "queen" or "king")
 				total += 10
 			else
 				total += value
@@ -35,6 +104,7 @@ helpers do
 				total -= (ace_count-1)*10
 			end
 		end
+		return total
 	end
 end
 
@@ -51,17 +121,68 @@ post '/new_player' do
 		@error = "You must enter your name."
 		halt erb(:new_player)
 	end
-	# Do setup tasks
+	# Do initialization tasks
 	session[:player_name] = params[:name]
 	session[:player_score] = 0
 	session[:player_wins] = 0
+	session[:player_status] = 0
+	session[:dealer_score] = 0
+	session[:dealer_status] = 0
+	session[:current_winner] = nil
+	# Do setup tasks
+	setup_game
+	# Redirect to gameplay
 	redirect '/game'
 end
 
 get '/game' do
+	session[:player_score] = calculate_total(session[:player_hand])
+	if session[:player_score] == 21 and session[:player_hand].length == 2
+		session[:player_status] = 1
+	elsif session[:player_score] == 21
+		session[:player_status] = 2
+	elsif session[:player_score] > 21
+		session[:player_status] = 4
+	end
+	session[:dealer_score] = calculate_total(session[:dealer_hand])
+	if session[:dealer_score] == 21 and session[:dealer_hand].length == 2
+		session[:dealer_status] = 1
+	elsif session[:dealer_score] == 21
+		session[:dealer_status] = 2
+	elsif session[:dealer_score] > 21
+		session[:dealer_status] = 4
+	end
+
+	determine_winner
+
 	erb :game
 end
 
 post '/game' do
+	# Do player stuff based on what button they hit
+	if params[:action] == "Hit"
+		session[:player_hand] += session[:deck].pop(1)
+	elsif params[:action] == "Stay"
+		session[:player_status] = 3
+	end
+
+	# Dealer turn
+	if session[:dealer_score] < 17
+		session[:dealer_hand] += session[:deck].pop(1)
+	else
+		session[:dealer_status] = 3
+	end
+	erb :game
+end
+
+post '/new_game' do
+	session[:player_score] = 0
+	session[:dealer_score] = 0
+	session[:player_status] = 0
+	session[:dealer_status] = 0
+	session[:current_winner] = nil
+
+	setup_game
+
 	erb :game
 end
